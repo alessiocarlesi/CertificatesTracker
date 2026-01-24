3c64bdcbaa61de7c53a148d5e322f21188063b06
Alessio Alessio <alessiocarlesi@gmail.com>
2026-01-24 18:32:20 +0100

    Rimozione file zip di backup e cartella TempZip

--- TempZip/TempZip/TempZip/TempZip/TempZip/TempZip/TempZip/TempZip/TempZip/TempZip/TempZip/TempZip/app/src/main/java/com/example/certificatestracker/ApiKeys.kt
+++
@@ -1,9 +0,0 @@
-package com.example.certificatestracker
-
-object ApiKeys {
-    const val TWELVEDATA = "c021aeeca93343a7a72eaca2eb62f8e6"
-    const val MARKETSTACK = "e1e60f41a11968b889595584e0a6c310"
-    const val ALPHAVANTAGE = "35Y3470KIRAJIMIC"
-    const val STOCKDATA = "sQyjmDtxzY0xZfT6lDy5M66bQLQaeloE2BJAVybP"
-}
-
--- TempZip/TempZip/TempZip/TempZip/TempZip/TempZip/TempZip/TempZip/app/src/main/java/com/example/certificatestracker/FetchResult.kt
+++
@@ -1,6 +0,0 @@
-package com.example.certificatestracker
-
-sealed class FetchResult {
-    data class Success(val price: Double) : FetchResult()
-    data class Error(val message: String) : FetchResult()
-}
